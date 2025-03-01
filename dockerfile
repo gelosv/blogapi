e46359fba422a1d13d7bcea260c4f588f6059b87
Angelo Salazar <ange.devc@gmail.com>
2025-03-01 12:06:54 -0500

    add dockerfile

---
+++ dockerfile
@@ -0,0 +1,10 @@
+FROM node:22-alpine
+
+WORKDIR /usr/src/app
+
+COPY package.json ./
+COPY package-lock.json ./
+
+RUN npm i
+
+COPY . .
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import { Router } from 'express';
+import helmet from 'helmet';
+import { rateLimit } from 'express-rate-limit'
 import { router as routerPost } from './posts/posts.router'
 import { router as routerUser } from './users/user.router';
 import { router as authRouter } from './auth/auth.router'
@@ -9,20 +11,29 @@ import './auth/strategies'
 export function main() {
   const app = express();
   const router = Router();
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+    legacyHeaders: false,
+    handler: (_, res) => {
+      res.status(429).json({
+        statusCode: 429,
+        message: 'too many requests'
+      })
+    }
+  })
 
   app.set('port', process.env.PORT || 3000);
 
   app.use(express.json())
   app.use(express.urlencoded({extended: false}));
+  app.use(helmet())
+  app.use(limiter)
 
   app.use('/api', router)
   router.use('/posts', routerPost)
   router.use('/users', routerUser)
   router.use('/auth', authRouter)
-
-  app.get('/', (req, res) => {
-    res.json('SIUUUU');
-  })
 
   app.use(errorHandler)
 
