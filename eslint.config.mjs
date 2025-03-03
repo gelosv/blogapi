5da0aedc14a61c987f2634633a9e25370e3660ce
Angelo Salazar <ange.devc@gmail.com>
2025-03-03 16:27:27 -0500

    change eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,7 +23,7 @@ export default tseslint.config(
   tseslint.configs.recommended,
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-explicit-any": "error",
       "@typescript-eslint/no-unused-vars": "warn"
     }
   }
